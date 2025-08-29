350cb12bfbff43fafbe1297452eb45d226202f3f
Manideep <deepanddipyo@gmail.com>
2025-08-29 23:01:21 +0530

    routes are changed according to the token, new components added in dashboard

--- src/features/dashboard/Home.jsx
+++ src/features/dashboard/Home.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import Navigation from "./components/Navigation"
+import { ASSETS } from "../../utils/GetAssets"
+import Dashboard from "./components/Dashboard"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <main className="relative md:grid lg:grid-cols-[350px_1fr] md:grid-cols-[280px_1fr] w-full h-screen items-center justify-center">
+
+      {/* Background Image */}
+      <div className="absolute h-full w-full -z-9 object-cover">
+      </div>
+      <img
+        src={ASSETS.background}
+        alt="Background"
+        className="absolute h-full w-full -z-10 object-cover blur-[150px]"
+      />
+
+      <Navigation />
+      <Dashboard />
+    </main>
   )
 }
 
