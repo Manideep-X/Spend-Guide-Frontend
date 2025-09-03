8905dd5eb6964dd8d3b48a3ed6eba1ffd3a2175e
Manideep <deepanddipyo@gmail.com>
2025-09-04 00:01:54 +0530

    service layers for each features are created

--- src/services/IncomeService.jsx
+++ src/services/IncomeService.jsx
@@ -1,12 +1,50 @@
-const saveNewIncome = async () => {
+import { API_ENDPOINTS } from "../utils/EndpointsConfig";
+import { GetReqHeader } from "../utils/GetReqHeader";
+import InExErrorHandling from "../utils/InExErrorHandling";
+
+const saveNewIncome = async ({ name, iconUrl, date, amount }) => {
     
+    const res = await fetch(API_ENDPOINTS.income, {
+        method: "POST",
+        headers: GetReqHeader(),
+        body: JSON.stringify({ name, iconUrl, date, amount })
+    });
+
+    // Error handling for different status code
+    const resOK = await InExErrorHandling({ res, expenseOrIncome: "income", isItFilter: false });
+
+    // This will return the json body of the response
+    return await resOK.json();
+
 }
 
 const getIncomesForCurrMonth = async () => {
 
+    const res = await fetch(API_ENDPOINTS.income, {
+        method: "GET",
+        headers: GetReqHeader()
+    });
+
+    // Error handling for different status code
+    const resOK = await InExErrorHandling({ res, expenseOrIncome: "income", isItFilter: false });
+
+    // This will return the json body of the response
+    return await resOK.json();
+
 }
 
-const deleteAnIncomeById = async () => {
+const deleteAnIncomeById = async ({ id }) => {
+
+    const res = await fetch(`${API_ENDPOINTS.income}/${id}`, {
+        method: "GET",
+        headers: GetReqHeader()
+    });
+
+    // Error handling for different status code
+    const resOK = await InExErrorHandling({ res, expenseOrIncome: "income", isItFilter: false });
+
+    // This will return the json body of the response
+    return await resOK.json();
 
 }
 
